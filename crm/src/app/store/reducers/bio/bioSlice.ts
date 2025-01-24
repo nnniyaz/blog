d29a4e8b49e7dbc9cd7ae95b966655829af18fbf
Niyaz Nassyrov <nassyrovich@gmail.com>
2025-01-23 19:15:54 -0500

    BLOG-9 Add bio redux layer

---
+++ crm/src/app/store/reducers/bio/bioSlice.ts
@@ -0,0 +1,38 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+import {Bio} from '../../../../domain/bio'
+
+type State = {
+    bio: Bio | null
+    isLoadingGetBio: boolean
+}
+
+const initialState: State = {
+    bio: null,
+    isLoadingGetBio: false
+}
+
+type SetBioAction = {
+    type: string
+    payload: Bio | null
+}
+
+type SetIsLoadingGetBioAction = {
+    type: string
+    payload: boolean
+}
+
+export const bioSlice = createSlice({
+    name: 'bio',
+    initialState: initialState,
+    reducers: {
+        setBio: (state, action: SetBioAction) => {
+            state.bio = action.payload
+        },
+        setIsLoadingGetBio: (state, action: SetIsLoadingGetBioAction) => {
+            state.isLoadingGetBio = action.payload
+        }
+    }
+})
+
+export default bioSlice.reducer
