ee7521acac4d1d9db0285d13848e65376ab80247
Niyaz Nassyrov <nassyrovich@gmail.com>
2025-01-23 19:15:40 -0500

    BLOG-9 Add author redux layer

---
+++ crm/src/app/store/reducers/author/authorSlice.ts
@@ -0,0 +1,38 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+import {Author} from '../../../../domain/author'
+
+type State = {
+    author: Author | null
+    isLoadingGetAuthor: boolean
+}
+
+const initialState: State = {
+    author: null,
+    isLoadingGetAuthor: false
+}
+
+type SetAuthorAction = {
+    type: string
+    payload: Author | null
+}
+
+type SetIsLoadingGetAuthorAction = {
+    type: string
+    payload: boolean
+}
+
+export const authorSlice = createSlice({
+    name: 'author',
+    initialState: initialState,
+    reducers: {
+        setAuthor: (state, action: SetAuthorAction) => {
+            state.author = action.payload
+        },
+        setIsLoadingGetAuthor: (state, action: SetIsLoadingGetAuthorAction) => {
+            state.isLoadingGetAuthor = action.payload
+        }
+    }
+})
+
+export default authorSlice.reducer
