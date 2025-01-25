4dac28163ea0978aadfb836a9af1b7121b819189
Niyaz Nassyrov <nassyrovich@gmail.com>
2025-01-24 19:29:11 -0500

    BLOG-18 Add auth service

---
+++ crm/src/http/auth/authService.ts
@@ -0,0 +1,19 @@
+import {AxiosResponse} from 'axios'
+
+import {$api, ApiRoutes, Request} from '../index'
+import {ErrorResponse, SuccessResponse} from '../response/response'
+
+type AuthServiceLoginIn = {
+    email: string;
+    password: string;
+}
+
+export class AuthService {
+    static async login(request: Request<AuthServiceLoginIn>): Promise<AxiosResponse<SuccessResponse<null> | ErrorResponse>> {
+        return $api(request.lang).post<SuccessResponse<null> | ErrorResponse>(ApiRoutes.AUTH_LOGIN, request.body)
+    }
+
+    static async logout(request: Request<null>): Promise<AxiosResponse<SuccessResponse<null> | ErrorResponse>> {
+        return $api(request.lang).post<SuccessResponse<null> | ErrorResponse>(ApiRoutes.AUTH_LOGOUT, null)
+    }
+}
