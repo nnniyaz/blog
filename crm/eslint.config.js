6e58131635fd44afecc679f18a192c79336f7496
Niyaz Nassyrov <nassyrovich@gmail.com>
2025-01-24 19:27:20 -0500

    BLOG-18 Refactor eslint

--- crm/eslint.config.js
+++ crm/eslint.config.js
@@ -6,35 +6,36 @@ import tseslint from 'typescript-eslint'
 import importPlugin from 'eslint-plugin-import'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    {ignores: ['dist']},
+    {
+        extends: [js.configs.recommended, ...tseslint.configs.recommended],
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+        },
+        plugins: {
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+            'import': importPlugin,
+        },
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            'react-refresh/only-export-components': [
+                'warn',
+                {allowConstantExport: true},
+            ],
+            'quotes': ['error', 'single', {'avoidEscape': true}],
+            'semi': ['error', 'never'],
+            'import/order': [
+                'error',
+                {
+                    'groups': [['builtin', 'external', 'internal']],
+                    'newlines-between': 'always',
+                    'alphabetize': {'order': 'asc', 'caseInsensitive': true},
+                },
+            ],
+            // Removed '@typescript-eslint/no-duplicate-enum-values' rule
+        },
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-      'import': importPlugin,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-      'quotes': ['error', 'single', { 'avoidEscape': true }],
-      'semi': ['error', 'never'],
-      'import/order': [
-          'error',
-          {
-              'groups': [['builtin', 'external', 'internal']],
-              'newlines-between': 'always',
-              'alphabetize': { 'order': 'asc', 'caseInsensitive': true },
-          },
-      ],
-    },
-  },
 )
