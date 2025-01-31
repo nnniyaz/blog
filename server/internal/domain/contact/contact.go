fdf3e9def3b26a73638bf96e6768fbc3989332d6
Niyaz Nassyrov <nassyrovich@gmail.com>
2025-01-31 16:45:12 -0500

    BLOG-15 Change contact's string to mlString

--- server/internal/domain/contact/contact.go
+++ server/internal/domain/contact/contact.go
@@ -2,24 +2,24 @@ package contact
 
 import (
 	"github.com/nnniyaz/blog/internal/domain/base/uuid"
-	exceptions2 "github.com/nnniyaz/blog/internal/domain/contact/exceptions"
+	"github.com/nnniyaz/blog/pkg/core"
 	"strings"
 	"time"
 )
 
 type Contact struct {
 	id        uuid.UUID
-	label     string
+	label     core.MlString
 	link      string
 	isDeleted bool
 	createdAt time.Time
 	updatedAt time.Time
 }
 
-func NewContact(label, link string) (*Contact, error) {
-	cleanedLabel := strings.TrimSpace(label)
-	if cleanedLabel == "" {
-		return nil, exceptions2.ErrLabelEmpty
+func NewContact(label core.MlString, link string) (*Contact, error) {
+	cleanedLabel, err := label.Clean()
+	if err != nil {
+		return nil, err
 	}
 
 	return &Contact{
@@ -36,7 +36,7 @@ func (c *Contact) GetId() uuid.UUID {
 	return c.id
 }
 
-func (c *Contact) GetLabel() string {
+func (c *Contact) GetLabel() core.MlString {
 	return c.label
 }
 
@@ -56,10 +56,10 @@ func (c *Contact) GetUpdatedAt() time.Time {
 	return c.updatedAt
 }
 
-func (c *Contact) Update(label, link string) error {
-	cleanedLabel := strings.TrimSpace(label)
-	if cleanedLabel == "" {
-		return exceptions2.ErrLabelEmpty
+func (c *Contact) Update(label core.MlString, link string) error {
+	cleanedLabel, err := label.Clean()
+	if err != nil {
+		return err
 	}
 
 	c.label = cleanedLabel
@@ -69,7 +69,7 @@ func (c *Contact) Update(label, link string) error {
 	return nil
 }
 
-func UnmarshalContactFromDatabase(id uuid.UUID, label, link string, isDeleted bool, createdAt, updatedAt time.Time) *Contact {
+func UnmarshalContactFromDatabase(id uuid.UUID, label core.MlString, link string, isDeleted bool, createdAt, updatedAt time.Time) *Contact {
 	return &Contact{
 		id:        id,
 		label:     label,
